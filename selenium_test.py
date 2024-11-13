56ae1cf9c75863b734f9946c16ebe193b0be5725
talevi83 <talevi83@gmail.com>
2024-11-13 16:19:39 +0200

    Add jenkinsfile

--- selenium_test.py
+++ selenium_test.py
@@ -1,10 +1,20 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from time import sleep
 import os
 
 class TestTipCalculator(unittest.TestCase):
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    service = Service('/usr/local/bin/chromedriver')
+    driver = webdriver.Chrome(service=service, options=options)
+
     @classmethod
     def setUpClass(cls):
         # Initialize the Chrome WebDriver
