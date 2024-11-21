eb0e2da467675a8577d70b3aca9d2c90e74e3d4a
talevi83 <38805893+talevi83@users.noreply.github.com>
2024-11-21 19:38:11 +0200

    Test (#1)
    * test
    
    * test

--- second.py
+++ second.py
@@ -1,3 +1,6 @@
 def second_func():
     a = int(input("Enter a number: "))
     return a - 1
+
+def bla():
+    print("bla")
--- test.py
+++ test.py
@@ -6,6 +6,6 @@ driver_path = "/Users/tallevi/chromedriver/chromedriver"
 
 driver = webdriver.Chrome(service=webdriver.chrome.service.Service(driver_path))
 
-driver.get("https://www.google.com")
+driver.get("https://gmail.com")
 
 driver.quit()
